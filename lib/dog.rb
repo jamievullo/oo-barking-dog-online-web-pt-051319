ee282c4d195f5b51fe9b7b1d2b115a1288a5e612
Jamie Vullo <jvullo@protonmail.com>
2019-07-15 13:15:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 class Dog 
-  def name= (dogs_name)
-    @this_dogs_name = dogs_name
+  
+  def name=(name)
+    @name = name
   end
   
   def name 
-    @this_dogs_name
+    @name
   end
   
-  def bark
+  def bark 
     puts "woof!"
   end
 end
